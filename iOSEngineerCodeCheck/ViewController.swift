c62db83a1ebb5a9f1ae432459589f95e38e6cd34
Ryosuke1025 <ryosuke131025@icloud.com>
2022-09-12 16:26:20 +0900

    [Fix] 改行、空白、スペース、インデント、命名の修正を行いました

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -9,21 +9,19 @@
 import UIKit
 
 class ViewController: UITableViewController, UISearchBarDelegate {
-
-    @IBOutlet weak var SchBr: UISearchBar!
+    @IBOutlet weak var searchbar: UISearchBar!
     
-    var repo: [[String: Any]]=[]
-    
+    var repository: [[String: Any]]=[]
     var task: URLSessionTask?
     var word: String!
     var url: String!
-    var idx: Int!
+    var index: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
+        searchbar.text = "GitHubのリポジトリを検索できるよー"
+        searchbar.delegate = self
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -37,7 +35,6 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        
         word = searchBar.text!
         
         if word.count != 0 {
@@ -45,7 +42,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
             task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
                 if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = obj["items"] as? [[String: Any]] {
-                    self.repo = items
+                    self.repository = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
@@ -55,38 +52,31 @@ class ViewController: UITableViewController, UISearchBarDelegate {
         // これ呼ばなきゃリストが更新されません
         task?.resume()
         }
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
         if segue.identifier == "Detail"{
             let dtl = segue.destination as! ViewController2
             dtl.vc1 = self
         }
-        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+        return repository.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
+        let rp = repository[indexPath.row]
         cell.textLabel?.text = rp["full_name"] as? String ?? ""
         cell.detailTextLabel?.text = rp["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
-        
     }
-    
 }
