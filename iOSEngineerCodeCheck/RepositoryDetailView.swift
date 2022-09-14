fdf3abc5da60aeaac3f4c86f0801477415daf38d
Ryosuke1025 <ryosuke131025@icloud.com>
2022-09-14 18:49:22 +0900

    [Add] Swiftlintの導入

--- iOSEngineerCodeCheck/RepositoryDetailView.swift
+++ iOSEngineerCodeCheck/RepositoryDetailView.swift
@@ -16,14 +16,14 @@ class RepositoryDetailView: UIViewController {
     @IBOutlet weak var wachers: UILabel!
     @IBOutlet weak var forks: UILabel!
     @IBOutlet weak var issues: UILabel!
-    
-    var RepositoryList: RepositoryListView!
-    
+
+    var repositoryList: RepositoryListView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let repo = RepositoryList.repository[RepositoryList.index]
-        
+
+        let repo = repositoryList.repository[repositoryList.index]
+
         language.text = "Written in \(repo["language"] as? String ?? "")"
         stargazers.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
         wachers.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
@@ -31,15 +31,15 @@ class RepositoryDetailView: UIViewController {
         issues.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         getImage()
     }
-    
+
     func getImage() {
-        let repo = RepositoryList.repository[RepositoryList.index]
-        
+        let repo = repositoryList.repository[repositoryList.index]
+
         repositoryName.text = repo["full_name"] as? String
-        
+
         if let owner = repo["owner"] as? [String: Any] {
             if let imgURL = owner["avatar_url"] as? String {
-                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { data, _, _ in
                     let img = UIImage(data: data!)!
                     DispatchQueue.main.async {
                         self.imageView.image = img
