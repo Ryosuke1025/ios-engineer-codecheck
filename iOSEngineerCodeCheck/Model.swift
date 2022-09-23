583d29b86f6d27dca5e91b4af9feef5531975763
Ryosuke1025 <ryosuke131025@icloud.com>
2022-09-23 23:22:59 +0900

    [Add]Codableに準拠した構造体を追加

---
+++ iOSEngineerCodeCheck/Model.swift
@@ -0,0 +1,49 @@
+//  Model.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 須崎良祐 on 2022/09/23.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct SearchRepositories: Codable {
+    
+    let items: [Repository]
+    
+    private enum CodingKeys: String, CodingKey {
+        case items
+    }
+}
+
+struct Repository: Codable {
+    
+    let id: Int
+    let fullName: String
+    let language: String
+    let stargazersCount: Int
+    let watchersCount: Int
+    let forksCount: Int
+    let openIssuesCount: Int
+    
+    let owner: Owner
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case fullName = "full_name"
+        case language = "language"
+        case stargazersCount = "stargazers_count"
+        case watchersCount = "watchers_count"
+        case forksCount = "forks_count"
+        case openIssuesCount = "open_issues_count"
+        case owner = "owner"
+    }
+}
+
+struct Owner: Codable {
+    let avatarUrl: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
+}
